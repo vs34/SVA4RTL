d0926c36377184a98a9539d90b25e34838e92b22
Server <hi.vaibhavsingh@gmail.com>
2026-03-25 23:05:54 +0530

    Server_2026-03-25_23:05:54

---
+++ jgproject_run_pipeline_and_micro_architecture_2/sessionLogs/session_0/jg_session_0.cmd
@@ -0,0 +1,12 @@
+#----------------------------------------
+# JasperGold Version Info
+# tool      : JasperGold 2019.09
+# platform  : Linux 2.6.32-754.35.1.el6.x86_64
+# version   : 2019.09p002 64 bits
+# build date: 2019.11.26 18:17:27 PST
+#----------------------------------------
+# started Wed Mar 25 23:05:07 IST 2026
+# hostname  : edatools-server2.iiitd.edu.in
+# pid       : 58011
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:41500' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_pipeline_and_micro_architecture_2/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_pipeline_and_micro_architecture_2/.tmp/.initCmds.tcl' '/home/vaibhav22555/Desktop/SVA4RTL/input/run_pipeline_and_micro_architecture_2.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_pipeline_and_micro_architecture_2/.tmp/.postCmds.tcl'
+analyze -sv09 sep_pkg.sv
---
+++ jgproject_run_reset_behavior_1/sessionLogs/session_0/jg_session_0.cmd
@@ -0,0 +1,12 @@
+#----------------------------------------
+# JasperGold Version Info
+# tool      : JasperGold 2019.09
+# platform  : Linux 2.6.32-754.35.1.el6.x86_64
+# version   : 2019.09p002 64 bits
+# build date: 2019.11.26 18:17:27 PST
+#----------------------------------------
+# started Wed Mar 25 23:05:29 IST 2026
+# hostname  : edatools-server2.iiitd.edu.in
+# pid       : 58929
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:40276' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_1/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_1/.tmp/.initCmds.tcl' '/home/vaibhav22555/Desktop/SVA4RTL/input/run_reset_behavior_1.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_1/.tmp/.postCmds.tcl'
+analyze -sv09 sep_pkg.sv
