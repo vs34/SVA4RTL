a6f1699ecd08202d48b73a5438cd0b1fac919832
Vaibhav Singh <hi.vaibhavsingh@gmail.com>
2026-03-25 23:29:38 +0530

    try again

--- input/run_reset_behavior_0.tcl
+++ input/run_reset_behavior_0.tcl
@@ -1,6 +1,6 @@
 analyze -sv09 mbox_ctrl.sv
 analyze -sv09 mbox_pkg.sv
-analyze -sv09 mbox_ctrl_reset_behavior_assert.sv
+analyze -sv09 assert_reset_behavior_0.sv
 elaborate -top mbox_ctrl
 clock clk
 reset -expression {!rst_n}
