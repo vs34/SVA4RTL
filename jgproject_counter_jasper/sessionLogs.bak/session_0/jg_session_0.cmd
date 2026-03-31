0b7defc27d25d2b7c754bf35916545b6b2a69b32
Server <hi.vaibhavsingh@gmail.com>
2026-03-31 12:07:23 +0530

    Server_2026-03-31_12:07:23

--- jgproject_counter_jasper/sessionLogs.bak/session_0/jg_session_0.cmd
+++ jgproject_counter_jasper/sessionLogs.bak/session_0/jg_session_0.cmd
@@ -5,10 +5,10 @@
 # version   : 2019.09p002 64 bits
 # build date: 2019.11.26 18:17:27 PST
 #----------------------------------------
-# started Mon Mar 30 22:18:39 IST 2026
+# started Mon Mar 30 22:29:11 IST 2026
 # hostname  : edatools-server2.iiitd.edu.in
-# pid       : 34720
-# arguments : '-label' 'session_0' '-console' '//127.0.0.1:43625' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/.tmp/.initCmds.tcl' 'counter_jasper.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/.tmp/.postCmds.tcl'
+# pid       : 48241
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:42132' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/.tmp/.initCmds.tcl' 'counter_jasper.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/.tmp/.postCmds.tcl'
 # JasperGold FPV run script for counter.sv
 # Usage: jg -fpv -batch -tcl counter_jasper.tcl
 
