80d506b342fda23023ffe19cedfabfaec56077f2
Server <hi.vaibhavsingh@gmail.com>
2026-03-31 21:31:05 +0530

    Server_2026-03-31_21:31:05

--- jgproject_counter_jasper/sessionLogs/session_0/jg_session_0.cmd
+++ jgproject_counter_jasper/sessionLogs/session_0/jg_session_0.cmd
@@ -5,10 +5,10 @@
 # version   : 2019.09p002 64 bits
 # build date: 2019.11.26 18:17:27 PST
 #----------------------------------------
-# started Tue Mar 31 12:06:55 IST 2026
+# started Tue Mar 31 21:30:42 IST 2026
 # hostname  : edatools-server2.iiitd.edu.in
-# pid       : 58842
-# arguments : '-label' 'session_0' '-console' '//127.0.0.1:38946' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/.tmp/.initCmds.tcl' 'counter_jasper.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/.tmp/.postCmds.tcl'
+# pid       : 2940
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:45201' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/.tmp/.initCmds.tcl' 'counter_jasper.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_counter_jasper/.tmp/.postCmds.tcl'
 # JasperGold FPV run script for counter.sv
 # Usage: jg -fpv -batch -tcl counter_jasper.tcl
 
--- jgproject_run_reset_behavior_0/sessionLogs.bak/session_0/jg_session_0.cmd
+++ jgproject_run_reset_behavior_0/sessionLogs.bak/session_0/jg_session_0.cmd
@@ -5,10 +5,10 @@
 # version   : 2019.09p002 64 bits
 # build date: 2019.11.26 18:17:27 PST
 #----------------------------------------
-# started Mon Mar 30 22:29:20 IST 2026
+# started Tue Mar 31 12:07:05 IST 2026
 # hostname  : edatools-server2.iiitd.edu.in
-# pid       : 48615
-# arguments : '-label' 'session_0' '-console' '//127.0.0.1:38854' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_0/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_0/.tmp/.initCmds.tcl' 'run_reset_behavior_0.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_0/.tmp/.postCmds.tcl'
+# pid       : 59292
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:46604' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_0/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_0/.tmp/.initCmds.tcl' 'run_reset_behavior_0.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_0/.tmp/.postCmds.tcl'
 analyze -sv09 mbox_pkg.sv
 analyze -sv09 mbox_ctrl.sv
 analyze -sv09 assert_reset_behavior_0.sv
