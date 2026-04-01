9252ea856e6f22a0e7725e217ce13dbddc36b309
Server <hi.vaibhavsingh@gmail.com>
2026-04-01 12:20:46 +0530

    Server_2026-04-01_12:20:46

--- jgproject_run_reset_behavior_1/sessionLogs/session_0/jg_session_0.cmd
+++ jgproject_run_reset_behavior_1/sessionLogs/session_0/jg_session_0.cmd
@@ -5,10 +5,10 @@
 # version   : 2019.09p002 64 bits
 # build date: 2019.11.26 18:17:27 PST
 #----------------------------------------
-# started Wed Apr 01 11:59:34 IST 2026
+# started Wed Apr 01 12:20:41 IST 2026
 # hostname  : edatools-server2.iiitd.edu.in
-# pid       : 56306
-# arguments : '-label' 'session_0' '-console' '//127.0.0.1:35599' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_1/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_1/.tmp/.initCmds.tcl' 'run_reset_behavior_1.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_1/.tmp/.postCmds.tcl'
+# pid       : 16063
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:41572' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AQAAADx/////AAAAAAAAA3oBAAAAEABMAE0AUgBFAE0ATwBWAEU=' '-proj' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_1/sessionLogs/session_0' '-init' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_1/.tmp/.initCmds.tcl' 'run_reset_behavior_1.tcl' '-hidden' '/home/vaibhav22555/Desktop/SVA4RTL/jgproject_run_reset_behavior_1/.tmp/.postCmds.tcl'
 analyze -sv09 mbox_pkg.sv
 analyze -sv09 mbox_ctrl.sv
 analyze -sv09 assert_reset_behavior_1.sv
