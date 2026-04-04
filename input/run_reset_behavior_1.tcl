d8d9cf0ba4734b53544174b15e4d9c2ba4919bb7
Vaibhav Singh <hi.vaibhavsingh@gmail.com>
2026-04-04 21:49:52 +0530

    input clean no extra compute

--- input/run_reset_behavior_1.tcl
+++
@@ -1,8 +0,0 @@
-analyze -sv09 mbox_pkg.sv
-analyze -sv09 mbox_ctrl.sv
-analyze -sv09 assert_reset_behavior_1.sv
-analyze -sv09 bind_reset_behavior_1.sv
-elaborate -top mbox_ctrl
-clock clk
-reset -expression {!rst_n}
-prove -all
